1181d0c6ba579ac5f8fdba1c6bcde67e33a65b90
maitrongnhan99 <mainhan.nm30@gmail.com>
2023-07-01 13:53:51 +0700

    add code passing all test case

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const formidable = require('formidable')
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+    res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', async (req, res) => {
+    const form = new formidable.IncomingForm()
+    await form.parse(req, function (err, fields, files) {
+        const { originalFilename, mimetype, size } = files.upfile[0]
 
+        res.send({
+            name: originalFilename,
+            type: mimetype,
+            size,
+        })
+    })
+})
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+    console.log('Your app is listening on port ' + port)
+})
